292385b95c348e9ad6d6a4b2580dca8f66a6fee1
elinoza <hillcakmak@gmail.com>
2020-12-17 19:05:22 +0300

    sum

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -6,7 +6,12 @@ const path = require("path")
 const uniqid = require("uniqid")
 const { readDB, writeDB } = require("../lib/utilities")
 const { check, validationResult } = require("express-validator")
+const { promisify } = require("util")
 const upload = multer({})
+const { begin } = require("xmlbuilder");
+const axios = require("axios")
+const { parseString } = require("xml2js")
+const asyncParser = promisify(parseString)
 
 const router = express.Router()
 const productsFolderPath = path.join(__dirname, "../../../public/img/products")
@@ -71,12 +76,12 @@ router.get("/", async(req, res,next) => {
   try{ 
       const productsDB =  await readDB(productsFilePath )
       if (req.query && req.query.name) {
-        const filteredproductss = productsDB.filter(
-          products =>
-            products.hasOwnProperty("name") &&
-            products.name.toLowerCase() === req.query.name.toLowerCase()
+        const filteredproducts = productsDB.filter(
+          product =>
+            product.hasOwnProperty("name") &&
+            product.name.toLowerCase() === req.query.name.toLowerCase()
         )
-        res.send(filteredproductss)
+        res.send(filteredproducts)
       } else {
         res.send(productsDB)
   }}
@@ -174,6 +179,58 @@ router.delete("/:id", async (req, res, next) => {
     next(error)
   }
 })
+
+/// GET  IDS GIVEN IN REQ.QUERY
+router.get("/sum/Two/Prices", async(req, res,next) => {
+try{
+const {id1,id2}= req.query
+const productsDB = await readDB(productsFilePath )
+  const pro1= productsDB.filter(product=> product._id === id1)
+  const pro2= productsDB.filter(product=> product._id === id2)
+
+const xmlBody = begin()
+.ele("soap:Envelope", {
+  "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance",
+  "xmlns:xsd":"http://www.w3.org/2001/XMLSchema" ,
+  "xmlns:soap":"http://schemas.xmlsoap.org/soap/envelope/",
+})
+.ele("soap:Body")
+.ele("Add ",  {
+  "xmlns": "http://tempuri.org/"
+})
+.ele("intA")
+.text(parseInt(pro1.price))
+.up()
+.ele("intB")
+.text(parseInt(pro2.price))
+.end();
+
+
+const response = await axios({
+  method: "post",
+  url:
+    "http://www.dneonline.com/calculator.asmx?op=Add",
+  data: xmlBody,
+  headers: { "Content-type": "text/xml" },
+})
+
+
+console.log(response)
+const parsedJS = await asyncParser(xml)//xml2json
+// res.send(parsedJS)
+res.send(
+  parsedJS["soap:Envelope"]["soap:Body"][0][
+    "m:AddResponse"
+  ][0]["m:AddResult"][0]
+)
+
+} catch (error) {
+  next(error)
+  }
+
+
+
+});
 
 
 module.exports = router
